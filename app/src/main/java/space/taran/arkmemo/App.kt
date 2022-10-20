8e4d59f201d18e98fa1d979718422ba07159ae43
ShubertMunthali <munthalimartinz@gmail.com>
2022-10-20 20:39:24 +0200

    Initial implementation

--- app/src/main/java/space/taran/arkmemo/App.kt
+++ app/src/main/java/space/taran/arkmemo/App.kt
@@ -3,29 +3,37 @@ package space.taran.arkmemo
 import android.app.Application
 import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.acra.config.dialog
 import org.acra.config.httpSender
 import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
 import org.acra.sender.HttpSender
+import space.taran.arkmemo.space.taran.arkmemo.utils.Config
 
 @HiltAndroidApp
 class App: Application() {
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
+        initAcra()
+    }
 
-        initAcra{
+    private fun initAcra() = CoroutineScope(Dispatchers.IO).launch {
+        val enabled = Config.newInstance(context = baseContext).crashReport
+        if (!enabled) return@launch
+
+        initAcra {
             buildConfigClass = BuildConfig::class.java
             reportFormat = StringFormat.JSON
-
-            dialog{
+            dialog {
                 text = getString(R.string.crash_dialog_desc)
                 title = getString(R.string.crash_dialog_title)
                 commentPrompt = getString(R.string.crash_dialog_comment)
             }
-
-            httpSender{
+            httpSender {
                 uri = BuildConfig.ACRA_URI
                 basicAuthLogin = BuildConfig.ACRA_LOGIN
                 basicAuthPassword = BuildConfig.ACRA_PASS
