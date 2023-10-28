baabfb16b0059a36666085a00194e697a189b63e
Hieu Vu <star.hieu.99@gmail.com>
2023-10-28 11:47:43 +0700

    Create module

--- app/src/main/java/space/taran/arkmemo/di/PreferencesModule.kt
+++ app/src/main/java/space/taran/arkmemo/di/PreferencesModule.kt
@@ -1,4 +1,15 @@
 package space.taran.arkmemo.di
 
-class PreferencesModule {
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import space.taran.arkmemo.preferences.MemoPreferences
+import space.taran.arkmemo.preferences.MemoPreferencesImpl
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class PreferencesModule {
+    @Binds
+    abstract fun bindRepository(impl: MemoPreferencesImpl): MemoPreferences
 }
--- app/src/main/java/space/taran/arkmemo/di/RepositoryModule.kt
+++ app/src/main/java/space/taran/arkmemo/di/RepositoryModule.kt
@@ -1,4 +1,16 @@
 package space.taran.arkmemo.di
 
-class RepositoryModule {
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import space.taran.arkmemo.data.repositories.TextNotesRepo
+import space.taran.arkmemo.data.repositories.TextNotesRepoImpl
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindRepository(impl: TextNotesRepoImpl): TextNotesRepo
 }
