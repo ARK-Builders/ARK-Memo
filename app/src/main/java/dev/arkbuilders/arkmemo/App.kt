4afacd7fe68bfc1c5bba149c83523e07ea5ed777
tuancoltech <tuancoltech@gmail.com>
2024-09-14 10:36:13 +0700

    [feat] Replace Acra with Firebase Crashlytics for crash monitoring: https://app.asana.com/0/1207819682524134/1208211226661157

--- app/src/main/java/dev/arkbuilders/arkmemo/App.kt
+++ app/src/main/java/dev/arkbuilders/arkmemo/App.kt
@@ -3,14 +3,6 @@ package dev.arkbuilders.arkmemo
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import dev.arkbuilders.arklib.initArkLib
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.acra.config.dialog
-import org.acra.config.httpSender
-import org.acra.data.StringFormat
-import org.acra.ktx.initAcra
-import org.acra.sender.HttpSender
 import dev.arkbuilders.arkfilepicker.folders.FoldersRepo
 import dev.arkbuilders.arkmemo.preferences.MemoPreferences
 import javax.inject.Inject
@@ -26,27 +18,5 @@ class App: Application() {
         System.loadLibrary("arklib")
         initArkLib()
         FoldersRepo.init(this)
-        initAcra()
-    }
-
-    private fun initAcra() = CoroutineScope(Dispatchers.IO).launch {
-        val enabled = memoPreferences.getCrashReportEnabled()
-        if (!enabled) return@launch
-
-        initAcra {
-            buildConfigClass = BuildConfig::class.java
-            reportFormat = StringFormat.JSON
-            dialog {
-                text = getString(R.string.crash_dialog_desc)
-                title = getString(R.string.crash_dialog_title)
-                commentPrompt = getString(R.string.crash_dialog_comment)
-            }
-            httpSender {
-                uri = BuildConfig.ACRA_URI
-                basicAuthLogin = BuildConfig.ACRA_LOGIN
-                basicAuthPassword = BuildConfig.ACRA_PASS
-                httpMethod = HttpSender.Method.POST
-            }
-        }
     }
 }
